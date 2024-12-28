31f5523116384bad8b8af8b91f493d59b4775ce1
Kiyoto Tanaka <tanaka.kiyoto56@gmail.com>
2024-12-28 19:33:41 +0900

    [add]webのDockerコンテナ起動を作成

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,7 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
@@ -10,6 +10,10 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  server: {
+    port: process.env.WEB_PORT,
+    host: true
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
