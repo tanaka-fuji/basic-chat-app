0ae8723713d00e6ce401a8cb540fd614f17e97cc
Kiyoto Tanaka <tanaka.kiyoto56@gmail.com>
2024-12-28 21:52:31 +0900

    [add]index.htmlの編集とNotFound表示の作成

---
+++ web/src/views/NotFound.vue
@@ -0,0 +1,10 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <h1>404 - Page Not Found</h1>
+    <p>The page you are looking for does not exist.</p>
+    <router-link to="/">Go Back to Home</router-link>
+  </div>
+</template>
