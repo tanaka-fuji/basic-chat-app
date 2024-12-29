1009ab643299044cc8c676a1056af5d9d9def589
Kiyoto Tanaka <tanaka.kiyoto56@gmail.com>
2024-12-29 14:53:03 +0900

    [add]vuetifyの導入

--- web/src/main.js
+++ web/src/main.js
@@ -4,7 +4,19 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import 'vuetify/styles'
+
 const app = createApp(App)
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+app.use(vuetify)
 
 app.use(router)
 
