1ea817956276087ab1e333ad07c5e2fcef320c21
Kiyoto Tanaka <tanaka.kiyoto56@gmail.com>
2024-12-29 15:57:46 +0900

    [update]不要なコンポーネントやview、cssファイルを削除、新たにviewファイルを追加

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,26 +1,24 @@
+import ChatView from '@/views/ChatView.vue'
+import NotFoundView from '@/views/NotFoundView.vue'
+import SigninView from '@/views/SigninView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import NotFound from '../views/NotFound.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView,
+      path: '/signin',
+      name: 'signin',
+      component: SigninView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/chat',
+      name: 'chat',
+      component: ChatView,
     },
     {
       path: '/:pathMatch(.*)*',
-      component: NotFound
+      component: NotFoundView
     },
   ],
 })
---
+++ web/src/views/ChatView.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <div><h1>チャットアプリ</h1></div>
+    <div>チャットアプリ</div>
+  </main>
+</template>
