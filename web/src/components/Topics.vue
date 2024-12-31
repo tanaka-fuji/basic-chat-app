23f5bd4da112be8b48a7337a28916b78561c71bc
Kiyoto Tanaka <tanaka.kiyoto56@gmail.com>
2024-12-31 12:21:34 +0900

    [add]トピック画面のデザインを追加

--- web/src/components/Topics.vue
+++ web/src/components/Topics.vue
@@ -1,8 +1,8 @@
 <script setup>
 import { inject, onBeforeMount, ref } from 'vue';
 
-const TopicCount = ref(0);
-const Topics = ref([]);
+const topicsCount = ref(0);
+const topics = ref([]);
 
 const httpClient = inject('httpClient');
 
@@ -10,17 +10,34 @@ onBeforeMount(async () => {
   try {
     const res = await httpClient.get('/topics');
     if (res.status === 200) {
-      TopicCount.value = res.data.count;
-      Topics.value = res.data.rows;
-      console.log(Topics.value);
+      topicsCount.value = res.data.count;
+      topics.value = res.data.rows;
+      console.log(topics.value);
       
     }
   } catch (error) {
-    console.log(error);
+    console.error(error);
   }
 });
+
+const fetchMessages = async (id) => {
+  try {
+    const res = await httpClient.get(`/topics/${id}/messages`);
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 </script>
 
 <template>
+  <div class="overflow-auto" style="background-color: white;height: 80vh;">
+    トピック数: {{ topicsCount }}
+    <v-card class="mt-1" height="100" variant="outlined" link @click="fetchMessages(topic.id)" v-for="(topic,i) in topics" :key="i">
+      <v-card-item>
+        <v-card-title>{{ topic.name }}</v-card-title>
+        <v-card-subtitle>{{ topic.description }}</v-card-subtitle>
+      </v-card-item>
+    </v-card>
+  </div>
 </template>
