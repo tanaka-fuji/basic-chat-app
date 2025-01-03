f159bc11363a5e37dedc8491c6f94d7e816aeeb1
Kiyoto Tanaka <tanaka.kiyoto56@gmail.com>
2025-01-03 17:58:04 +0900

    [add]socket.io-clientとmdi/fontを追加

--- web/src/main.js
+++ web/src/main.js
@@ -4,6 +4,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import '@mdi/font/css/materialdesignicons.css'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
@@ -12,6 +13,9 @@ import 'vuetify/styles'
 const app = createApp(App)
 
 const vuetify = createVuetify({
+  icons: {
+    defaultSet: 'mdi',
+  },
   components,
   directives,
 })
